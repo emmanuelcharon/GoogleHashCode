7a03802bfff6f22fa644f5db67bfc705f781a851
emmanuelcharon <emmanuelcharon@hotmail.com>
2018-02-07 12:02:22 +0100

    2018 practice inout files

---
+++ 2018/practice/practice.py
@@ -0,0 +1,19 @@
+"""
+Created on 7 Fev. 2018
+
+Python 3.6
+@author: emmanuelcharon
+"""
+
+import numpy as np
+import json
+import random
+
+
+import os
+
+
+class Pizza:
+  
+  def __init__(self, file_path):
+    pass
